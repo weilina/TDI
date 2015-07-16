a834b9098b2f4c8da4858bccfbdead4958da7078
weilina <fangwei.c@uma.es>
2015-07-16 20:26:30 +0200

    Create owl_constru.py

---
+++ owl_constru.py
@@ -0,0 +1,23 @@
+#coding: utf-8
+
+'''
+all terms are saved in "tdi_owl_list.pkl"
+'''
+
+TDI = ['NR','GI','TI','TLR','CHA','PEF','SE','NE','ATM']
+NR = [(u'天氣',u'氣候'),(u'自然'),(u'海灘'),(u'森林',u'樹木',u'花草')]
+NR_eng = ['weather','natural','beach','fauna']
+GI = [(u'交通',u'運輸'),(u'醫院'),(u'電信',u'電話',u'網路'),(u'商店',u'市場',u'超市',u'市集')]
+GI_eng = ['transport','health_service','telecommunication','commercial_facility']
+TI = [(u'住宿'),(u'餐廳'),(u'酒吧'),(u'諮詢')]
+TI_eng = ['accommodation','restaurant','bar','consultancy']
+TLR = [(u'遊樂園',u'遊樂場'),(u'運動',u'體育'),(u'購物',u'採購',u'採買'),(u'賭場'),(u'動物園')]
+TLR_eng = ['Theme parks','Sports','shopping','casino','zoo']
+CHA = [(u'博物館',u'美術館'),(u'古蹟'),(u'節慶',u'慶典'),(u'音樂會'),(u'展覽'),(u'手工藝'),(u'美食'),(u'民俗'),(u'宗教'),(u'習慣')]
+CHA_eng = ['museum','monument','festival', 'concerts', 'exposition','handicraft','gastronomy','folklore','religion','customs']
+PEF = [(u'政治'),(u'經濟'),(u'價格',u'價錢'),(u'治安')]
+PEF_eng = ['political','economic','price','safety']
+SE = [(u'居民',u'西班牙人'),(u'生活'),(u'語言',u'溝通')]
+SE_eng = ['resident','quality_of_life','communication']
+NE = [(u'風景',u'景色'),(u'景點'),(u'清潔'),(u'擁擠'),(u'污染')]
+NE_eng = ['scenery','attraction','cleanliness','overcrowding','pollution']
